c3127a9df1e8e78ee7471f925b59c56b877297bb
Xavier <xavistiles@gemail.com>
2023-06-11 11:51:54 +0200

    inicio

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController: ControllerBase
+    {
+        private List<User> users = new List<User> 
+        {
+            new User
+            {
+                id = 0,
+                name = "Test",
+                email = "Test",
+                mainImage = 1
+            },
+            new User
+            {
+                id = 1,
+                name = "Test",
+                email = "Test",
+                mainImage = 1
+            },new User
+            {
+                id = 3,
+                name = "Test",
+                email = "Test",
+                mainImage = 1
+            },
+
+        };
+
+        [HttpPost]
+        [Route("GetUsers")]
+        public dynamic GetUsers() 
+        {
+            return users;
+        }
+
+        [HttpPost]
+        [Route("SaveUser")]
+        public dynamic SaveUser(User user)
+        {
+            users.Add(user);
+            return users;
+        }
+
+        [HttpPost]
+        [Route("EditUser")]
+        public dynamic EditUser(User user)
+        {
+            User tmpUser = new User();
+            if (user.id < users.Count && (tmpUser = users[user.id]) != null)
+            {
+                users.Remove(tmpUser);
+                users.Add(user);
+                return users;
+            }
+            else 
+            {
+                return "no esta";
+            }
+            
+            
+        }
+    }
+}
---
+++ WebApi/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApi.Models
+{
+    public class User
+    {
+        public User(int _id,string _name, string _email, int _mainImage ) 
+        { 
+            id = _id;
+            name = _name;
+            email = _email;
+            mainImage = _mainImage;
+        }
+        public User() { }
+        public int id { get; set; }
+        public string name { get; set; } = "";
+        public string email { get; set; } = "";
+        public int mainImage { get; set; }
+    }
+}
